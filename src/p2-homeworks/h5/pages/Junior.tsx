e37535067b643475fabd27b6c979dbadecf08a17
SergioGord <gsricco@yandex.ru>
2022-05-30 22:06:46 +0300

    add homework5

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import s from './Junior.module.css'
+
+
+const Junior = () => {
+    return (
+        <div className={s.page}>
+            <h1 className={s.title}>Sorry...<br/>Page Junior<br/>is currently under development</h1>
+            <div><img  src="https://c.tenor.com/erbSWaDG1EUAAAAi/stitch-cute.gif" alt="error animation"/></div>
+            {/*<HW7/>*/}
+            {/*<HW8/>*/}
+            {/*<HW9/>*/}
+            {/*<HW10/>*/}
+            {/*<HW11/>*/}
+        </div>
+    );
+};
+
+export default Junior;
+
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Junior.module.css'
+
+
+const JuniorPlus = () => {
+    return (
+        <div className={s.page}>
+            <h1 className={s.title}>Sorry...<br/>Page Junior_Plus <br/>is currently under development too</h1>
+            <div><img src="https://c.tenor.com/erbSWaDG1EUAAAAi/stitch-cute.gif" alt="error animation"/></div>
+            {/*<HW12/>*/}
+            {/*<HW13/>*/}
+            {/*<HW14/>*/}
+            {/*<HW15/>*/}
+            {/*<HW16/>*/}
+        </div>
+    );
+};
+
+export default JuniorPlus;
