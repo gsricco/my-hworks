600c3408e18eb5db70f8c9f23d12eb877edcde1b
SergioGord <gsricco@yandex.ru>
2022-06-18 19:31:49 +0300

    add homework8

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Junior.module.css'
 import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
 
 
 const Junior = () => {
@@ -8,11 +9,12 @@ const Junior = () => {
         <div className={s.page}>
             {/* <h1 className={s.title}>Sorry...<br/>Page Junior<br/>is currently under development</h1>
             <div><img  src="https://c.tenor.com/erbSWaDG1EUAAAAi/stitch-cute.gif" alt="error animation"/></div>*/}
-            <HW7/>
-            {/*<HW8/>*/}
+            <div><HW7/></div>
+            <div><HW8/></div>
             {/*<HW9/>*/}
             {/*<HW10/>*/}
             {/*<HW11/>*/}
+
         </div>
     );
 };
